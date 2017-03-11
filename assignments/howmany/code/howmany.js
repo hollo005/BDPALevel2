92dbed8061fcd1b5199f08639cf71778e5d59da8
METROSTATE\36BDPA2 <imapkasiak04@gmail.com>
2017-03-11 13:53:28 -0600

    no message

---
+++ assignments/howmany/code/howmany.js
@@ -0,0 +1,17 @@
+function doLevel1(){
+	alert('hello')
+}
+function doLevel2(){
+	alert('hello')
+}
+function doLevel3(){
+	alert('hello')
+}
+  function show(id){
+	  var el = document.getElementById(id);
+      el.classList.remove("hidden");
+}
+    function hide(id){
+	  var el = document.getElementById(id);
+      el.classList.add("hidden");
+}
